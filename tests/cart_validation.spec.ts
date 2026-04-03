ef3015df70117247a4b5e88d5466cae224a257c2
Anil-R07 <anilgowda.r18@gmail.com>
2026-04-03 23:15:18 +0530

    Added Sort test

--- tests/cart_validation.spec.ts
+++ tests/cart_validation.spec.ts
@@ -9,29 +9,29 @@ test('login test', async ({ page }) => {
   await page.click('#login-button');
   //Assertion
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
-  await expect(page.locator('.title')).toHaveText('Products')
+  await expect(page.locator('.title')).toHaveText('Products');
   //add product to cart 
   await page
     .locator('.inventory_item')
     .filter({hasText:'Sauce Labs Backpack'})
     .getByText('Add to cart')
-    .click()
+    .click();
   await page
     .locator('.inventory_item')
     .filter({hasText:'Sauce Labs Bike Light'})
     .getByText('Add to cart')
-    .click()
+    .click();
   //Cart validation
-  const shopping_cart_badge= page.locator('.shopping_cart_badge')
-  await expect(shopping_cart_badge).toHaveText('2')
-  await shopping_cart_badge.click()
-  const quantity = page.locator('.cart_item')
-  await expect(quantity).toHaveCount(2)
+  const shopping_cart_badge= page.locator('.shopping_cart_badge');
+  await expect(shopping_cart_badge).toHaveText('2');
+  await shopping_cart_badge.click();
+  const quantity = page.locator('.cart_item');
+  await expect(quantity).toHaveCount(2);
   //Remove an item from cart 
   await page
     .locator('.cart_item')
     .filter({hasText:'Sauce Labs Backpack'})
     .getByRole('button', {name:'Remove'})
-    .click()
-  await expect(quantity).toHaveCount(1)
+    .click();
+  await expect(quantity).toHaveCount(1);
 });
---
+++ tests/sort_by_price.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test';
+
+test('sort by price', async({page}) => {
+    //Open Url
+    await page.goto('https://www.saucedemo.com/');
+    //Login
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+    //Assertion
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+    await expect(page.locator('.title')).toHaveText('Products')
+    //sort product 
+    await page.waitForSelector('.product_sort_container')
+    await page.locator('.product_sort_container').selectOption('lohi');
+
+    const first_item = await page.locator('.inventory_item').first();
+    const item_name = await first_item.locator('.inventory_item_name ').innerText();
+    await (first_item).getByText('Add to cart').click()
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+    await page.locator('.shopping_cart_link').click();
+
+    await expect(page.getByText(item_name)).toBeVisible()
+
+})
